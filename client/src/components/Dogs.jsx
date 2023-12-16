834b9384a2f09ecf71d064f984807564eb94a2bf
Ekow Yawson <ekowyawson71@gmail.com>
2023-12-15 19:14:11 -0500

    Initial commit of lab 15 Dogs.

---
+++ client/src/components/Dogs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Dogs(props) {
+
+  return (
+    <ul>
+      {
+        props.dogs.map((dog, idx) =>
+          <li key={dog._id}>{dog.name}</li>
+        )
+      }
+    </ul>
+  )
+}
+
+export default Dogs;
